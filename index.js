774c995c161c7f1cb9af6cc593fdae3669504f8d
David Pontes <orpelnet.dbp@gmail.com>
2025-01-13 13:17:03 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,37 +1,29 @@
-const fs = require('fs');
-const path = require('path');
-const xlsx = require('xlsx');
-const mysql = require('mysql2/promise');
-
-function formatCurrencyToDecimal(value) {
-    if (!value) {
-      console.error('Valor inválido ou indefinido:', value);
-      return 0;
-    }
-
-    let numericValue = value.replace(/R\$\s?/g, '').replace(/\./g, '').replace(/,/g, '.');
-    return parseFloat(numericValue);
-  }
+const fs = require("fs");
+const path = require("path");
+const xlsx = require("xlsx");
+const mysql = require("mysql2/promise");
 
 function excelDateToMySQL(serial) {
-    if (!serial || isNaN(serial)) {
-      console.error('Número serial inválido:', serial);
-      return null;
-    }
-  
-    try {
-      const excelEpoch = new Date(1900, 0, 1);
-      const adjustedDate = new Date(excelEpoch.getTime() + (serial - 1) * 86400000);
-      const year = adjustedDate.getFullYear();
-      const month = String(adjustedDate.getMonth() + 1).padStart(2, '0');
-      const day = String(adjustedDate.getDate()).padStart(2, '0');
-      return `${year}-${month}-${day}`;
-    } catch (error) {
-      console.error('Erro ao converter número serial para data:', error);
-      return null;
-    }
+  if (!serial || isNaN(serial)) {
+    console.error("Número serial inválido:", serial);
+    return null;
   }
-  
+
+  try {
+    const excelEpoch = new Date(1900, 0, 1);
+    const adjustedDate = new Date(
+      excelEpoch.getTime() + (serial - 1) * 86400000
+    );
+    const year = adjustedDate.getFullYear();
+    const month = String(adjustedDate.getMonth() + 1).padStart(2, "0");
+    const day = String(adjustedDate.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  } catch (error) {
+    console.error("Erro ao converter número serial para data:", error);
+    return null;
+  }
+}
+
 async function readExcelAndPersistToDB(filePath, tableName, dbConfig) {
   try {
     if (!fs.existsSync(filePath)) {
@@ -45,18 +37,19 @@ async function readExcelAndPersistToDB(filePath, tableName, dbConfig) {
     const jsonData = xlsx.utils.sheet_to_json(worksheet);
 
     const dataList = jsonData.map((row) => {
-
       return [
-        row['contrato'],
-        row['cpf'],
-        row['origem'],
-        (row['D8']),
-        excelDateToMySQL(row['venc']),
+        row["contrato"],
+        row["cpf"],
+        row["origem"],
+        row["D8"],
+        excelDateToMySQL(row["venc"]),
       ];
     });
 
     const connection = await mysql.createConnection(dbConfig);
-    
+
+    await connection.query("DELETE FROM tbconciliacaotodavida");
+
     const query = `INSERT INTO ${tableName} (contrato, cpf, origem, valorDesconto, dataCompetencia) VALUES ?`;
     await connection.query(query, [dataList]);
 
@@ -69,14 +62,14 @@ async function readExcelAndPersistToDB(filePath, tableName, dbConfig) {
 }
 
 const dbConfig = {
-  host: 'database-dev.ckqeyskgbzkh.us-east-2.rds.amazonaws.com',
-  user: 'dbuser_devcrm',
-  password: 'J3VG2Mo9kPje',
-  database: 'crm',
+  host: "database-dev.ckqeyskgbzkh.us-east-2.rds.amazonaws.com",
+  user: "dbuser_devcrm",
+  password: "J3VG2Mo9kPje",
+  database: "crm",
 };
 
-const filePath = path.join(__dirname, 'Pasta11.xlsx');
+const filePath = path.join(__dirname, "Pasta11.xlsx");
 
-const tableName = 'tbconciliacaotodavida';
+const tableName = "tbconciliacaotodavida";
 
 readExcelAndPersistToDB(filePath, tableName, dbConfig);
